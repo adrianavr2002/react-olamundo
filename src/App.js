06967c2905a9aee9bb46305727ccf971752d66c5
adrianavr2002 <adrianavr2002@yahoo.com.br>
2021-11-16 20:14:48 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Olá mundo!
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
